2996c639d21b23c38ed1ea04cb1734b9e5a4ed9d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-30 01:53:15 -0500

    This is way more elegant
    Oops. Math.
    
    lol
    
    ><
    
    omfg.
    
    Fix a damn typo
    
    Fix bad hp values
    
    I'm bad at math
    
    fuck a duck
    
    pet scaling math

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -6,7 +6,6 @@ sub EVENT_SPAWN
     if ($npc->IsPet() and $npc->GetOwner()->IsClient()) {  
        SAVE_PET_STATS($npc);
        UPDATE_PET_STATS($npc);
-       APPLY_EPIC_FOCI($npc);
     }
 }
 
@@ -14,7 +13,6 @@ sub EVENT_TICK
 {
     if ($npc->IsPet() and $npc->GetOwner()->IsClient()) {  
         UPDATE_PET_STATS($npc);
-        APPLY_EPIC_FOCI($npc);
     }
 }
 
@@ -59,10 +57,10 @@ sub UPDATE_PET_STATS
 
     if ($owner) {
         # Create Scalar. The max() probably isn't needed but just to be safe
-        my $pet_scalar = max(($owner->GetSpellDamage() / 125) + 1, 1);
+        my $pet_scalar = APPLY_FOCI($pet) * max(($owner->GetSpellDamage() / 100) + 1, 1);
 
         # Do max HP adjustment
-        my $max_hp = ceil($owner->GetBucket("max_hp") * $pet_scalar);
+        my $max_hp = ceil($owner->GetBucket("max_hp") * $pet_scalar); 
         $pet->ModifyNPCStat("max_hp", $max_hp);
 
         # Set spellscale and healscale for the pet
@@ -95,10 +93,8 @@ sub UPDATE_PET_STATS
         $pet->ModifyNPCStat("agi", $agi);
         $pet->ModifyNPCStat("atk", $atk);
 
-        # Set Min\Max Hits and Regen
-        my $min_hit = ceil($owner->GetBucket("min_hit") * $pet_scalar) . "";
-        my $max_hit = ceil($owner->GetBucket("max_hit") * $pet_scalar) . "";
-        my $hp_regen = ceil($owner->GetBucket("hp_regen") * $pet_scalar) . "";
+        $min_hit = ceil($owner->GetBucket("min_hit") * $pet_scalar) . "";
+        $max_hit = ceil($owner->GetBucket("max_hit") * $pet_scalar) . "";
 
         $pet->ModifyNPCStat("min_hit", $min_hit);
         $pet->ModifyNPCStat("max_hit", $max_hit);
@@ -110,7 +106,7 @@ sub UPDATE_PET_STATS
     }
 }
 
-sub APPLY_EPIC_FOCI 
+sub APPLY_FOCI 
 {
     my $pet = shift;
     my $owner = $pet->GetOwner()->CastToClient();
@@ -132,20 +128,13 @@ sub APPLY_EPIC_FOCI
                 $pet->CastSpell(847, $pet->GetID());
             }
 
-            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $mag_epic_scalar) . "";
-            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $mag_epic_scalar) . "";
-
-            $pet->ModifyNPCStat("min_hit", $min_hit);
-            $pet->ModifyNPCStat("max_hit", $max_hit);
-
-            # Do max HP adjustment
-            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $mag_epic_scalar);
-            $pet->ModifyNPCStat("max_hp", $max_hp);
         } elsif ($owner->GetBucket("epic_proc") eq "true") {
+            $owner->DeleteBucket("epic_proc");
             $pet->RemoveMeleeProc(849);
             $pet->RemoveMeleeProc(848);
-            $pet->BuffFadeBySpellID(847);
+            $pet->BuffFadeBySpellID(847);            
         }
+        return $mag_epic_scalar;
     }
 
     #Beastlord
@@ -159,20 +148,10 @@ sub APPLY_EPIC_FOCI
             if ($owner->GetBucket("epic_proc") ne "true") {                
                 $owner->SetBucket("epic_proc", "true");
                 # TODO CUSTOM PROC            
+            } elsif ($owner->GetBucket("epic_proc") eq "true") {
+                $owner->DeleteBucket("epic_proc");          
             }
-
-            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $bst_epic_scalar) . "";
-            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $bst_epic_scalar) . "";
-
-            $pet->ModifyNPCStat("min_hit", $min_hit);
-            $pet->ModifyNPCStat("max_hit", $max_hit);
-
-            # Do max HP adjustment and readjust current hp to original ratio
-            my $hp_ratio = $pet->GetHPRatio();
-            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $bst_epic_scalar);
-            my $cur_hp = ceil($max_hp * $hp_ratio);
-            $pet->ModifyNPCStat("max_hp", $max_hp);
-            $pet->SetHP($cur_hp);
+        return $bst_epic_scalar;
         }
     }
 
@@ -187,20 +166,11 @@ sub APPLY_EPIC_FOCI
             if ($owner->GetBucket("epic_proc") ne "true") {                
                 $owner->SetBucket("epic_proc", "true");
                 # TODO CUSTOM PROC            
+            } elsif ($owner->GetBucket("epic_proc") eq "true") {
+                $owner->DeleteBucket("epic_proc");
             }
-
-            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $nec_epic_scalar) . "";
-            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $nec_epic_scalar) . "";
-
-            $pet->ModifyNPCStat("min_hit", $min_hit);
-            $pet->ModifyNPCStat("max_hit", $max_hit);
-
-            # Do max HP adjustment and readjust current hp to original ratio
-            my $hp_ratio = $pet->GetHPRatio();
-            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $nec_epic_scalar);
-            my $cur_hp = ceil($max_hp * $hp_ratio);
-            $pet->ModifyNPCStat("max_hp", $max_hp);
-            $pet->SetHP($cur_hp);
+        return $nec_epic_scalar;
         }
     }
+    return 1;
 }
