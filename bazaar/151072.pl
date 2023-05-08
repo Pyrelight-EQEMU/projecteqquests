3612576a928f9934ebe5842bee01e6723963bd77
catapultam-habeo <catapultam-habeo@pyrelight.net>
2023-05-08 13:57:08 -0500

    Update 151072.pl

--- bazaar/151072.pl
+++ bazaar/151072.pl
@@ -2,7 +2,9 @@ sub EVENT_SAY
 {
     if ($client->GetGM()) {
         if ($text=~/hail/i) {
-            plugin::NPCTell("Hello");
+            if (!$client->GetBucket("FoSMet")) {
+                plugin::NPCTell("Greetings, young adventurer. I am Seshethkunaaz, Monarch of Dragons from a realm far beyond this meager existence. I desire to establish a dominion in this world and seek minions of exceptional skill and prowess. As I observe you, I cannot help but be intrigued by your potential, for I sense a [". quest::saylink("fos1a",1,"latent strength") ."] yearning to be awakened.");
+            }
         }
     }
 }
