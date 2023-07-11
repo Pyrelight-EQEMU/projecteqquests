e57c7640b09b7ded7ca8c861d14de8080d543175
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-07-11 12:53:53 -0500

    lol

--- nexus/Horender_Tevalian.pl
+++ nexus/Horender_Tevalian.pl
@@ -13,29 +13,58 @@ sub EVENT_SAY {
     }
   }
 
-  $teleport_zones{"The Northern Plains of Karana (Great Combine Spires)"} = ["northkarana", "The Northern Plains of Karana (Great Combine Spires)", 1215, -3690, -9, 0];
+  $teleport_zones{"The Northern Plains of Karana (Great Combine Spires [Default])"} = ["northkarana", "The Northern Plains of Karana (Great Combine Spires [Default])", 1215, -3690, -9, 0];
 
+  # Dark Elf
   if ($client->GetRace() == 6) {
-    $teleport_zones{"Neriak Commons (Home City)"} = ["neriakb", "Neriak Commons (Home City)", -562, -45, -39, 256];
+    $teleport_zones{"Neriak - Foreign Quarter (The Smuggler's Inn [Racial])"} = ["neriaka", "Neriak - Foreign Quarter (The Smuggler's Inn [Racial])", -91, 75, 16, 6];
   }
 
-  if ($client->GetRace == 1) {
-    $teleport_zones{"East Freeport (Home City)"} = ["freporte", "East Freeport (Home City)", -647, 218, -52, 287];
+  # Barbarian
+  if ($client->GetRace() == 2) {
+    $teleport_zones{"Halas (McDaniel's Smokes and Spirits [Racial])"} = ["halas", "Halas (McDaniel's Smokes and Spirits [Racial])", -319, 327, 3, 85];
   }
 
-  $teleport_zones{"Innothule Swamp (Blind)"} = ["innothule", "Innothule Swamp (Blind)", 620, -2370, -17, 35];
-  $teleport_zones{"Freeport Sewers (Blind)"} = ["freportw", "Freeport Sewers (Blind)", 40, -660, -32, 115];
-	
+  # Humans
+  if ($client->GetRace() == 1) {
+    $teleport_zones{"East Freeport (Freeport Inn [Racial])"} = ["freporte", "East Freeport (Freeport Inn [Racial])", -652, -49, -38, 388];
+  }
+
+  if ($client->GetRace() == 1 or $client->GetRace() == 330) {
+    $teleport_zones{"South Qeynos (Lion's Mane Inn [Racial])"} = ["qeynos", "South Qeynos (Lion's Mane Inn [Racial])", -64, 235, 3, 385];
+  }
+
+  # Druids & Rangers
+  if ($client->GetClass() == 6 or $client->GetClass() == 4) {
+    $teleport_zones{"Surefall Glade (The Grove [Class])"} = ["qrg", "Surefall Glade (The Grove [Class])", -230, -170, 2, 377];
+  }
+
+  # Trolls
+  if ($client->GetRace() == 9) {
+    $teleport_zones{"Grobb (Gunthak's Belch [Racial])"} = ["grobb", "Grobb (Gunthak's Belch [Racial])", -394, 53, 7, 70];
+  }
+
+  # Ogres
+  if ($client->GetRace() == 9) {
+    $teleport_zones{"Oggok (Oggok's Keep [Racial])"} = ["oggok", "Oggok (Oggok's Keep [Racial])", -245, 10, -8, 173];
+  }
+
+  # Frogloks
+  if ($client->GetRace() == 330) {
+    $teleport_zones{"Rathe Mountains (Gukta Refugees [Racial])"} = ["rathemtn", "Rathe Mountains (Gukta Refugees [Racial])", 103, -1793, 3, 448];
+  }
+
+  # Halflings
+  if ($client->GetRace() == 11) {
+    $teleport_zones{"Rivervale (Weary Foot Rest [Racial])"} = ["rivervale", "Rivervale (Weary Foot Rest [Racial])", -93, 187, 2, 128];
+  }
+
 
   if ($text=~/hail/i) {
-    if (quest::get_data($MAOcharKey) <= 0) {
-      plugin::NPCTell("Hail, traveler. You'll need to get permission to access the Nexus teleportation network. Speak to Magus Asorin on the central platform.");
-    } else {
-      plugin::NPCTell("Hail, traveler. I can transport you to the Great Spires on Antonica, or to any other location on the continent that you've become attuned to.");
-      $client->Message(257, " ------- Select a Destination ------- ");
-      foreach my $t (sort keys %teleport_zones) {
-        $client->Message(257, "-> ".quest::saylink($teleport_zones{$t}[1],0,$t));
-      }    
+    plugin::NPCTell("Hail, traveler. I can transport you to the Great Spires on Antonica, or to any other location on the continent that you've become attuned to.");
+    $client->Message(257, " ------- Select a Destination ------- ");
+    foreach my $t (sort keys %teleport_zones) {
+      $client->Message(257, "-> ".quest::saylink($teleport_zones{$t}[1],0,$t));
     }
   } elsif (exists($teleport_zones{$text}[1])) {
     $client->MovePC(quest::GetZoneID($teleport_zones{$text}[0]),$teleport_zones{$text}[2],$teleport_zones{$text}[3],$teleport_zones{$text}[4],$teleport_zones{$text}[5]);
