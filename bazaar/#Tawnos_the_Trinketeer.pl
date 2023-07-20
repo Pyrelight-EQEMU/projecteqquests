31930e4fc5a18a2f3bd4e897bc8eb2fb66982352
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-07-20 14:21:56 -0500

    iteration

--- bazaar/#Tawnos_the_Trinketeer.pl
+++ bazaar/#Tawnos_the_Trinketeer.pl
@@ -1,6 +1,15 @@
 sub EVENT_SAY {
+   my $response = "";
+   my $clientName = $client->GetCleanName();
+   my $link_epic = "[".quest::saylink("relic", true, "response_epic")."]";
+   my $link_custom = "[".quest::saylink("custom work", true, "response_custom")."]";
    if($text=~/hail/i) {
-      plugin::NPCTell("Hello!");
+      if (!$client->GetData("Tawnos")) {
+         $response = "Hail, $clientName. I am Tawnos, master artificer and enchanter! I am still setting up my facilities here in the Bazaar, but I can already offer some services. ";
+      } else {
+         $response = "Welcome back, $clientName. What can I do for you today? ";
+      }
+      $response = $response . "If you have acquired a $link_epic, I can offer you an corresponding ornament for it. If you are interested in $link_custom, we should talk!";
    }
 }
 
