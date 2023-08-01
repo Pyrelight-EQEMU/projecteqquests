e24e41e0ddb95b00cc429801797717f85c8d8f02
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-08-01 15:28:59 -0500

    test

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -1,8 +1,9 @@
 sub ProcessInstanceDialog {
     quest::debug("ProcessInstanceDialog Start");
 
-    my $text = plugin::val('text');
-    my $dz_zone = plugin::val('dz_zone');
+    my $text   = plugin::val('text');
+    my $client = plugin::val('client');
+    my $npc    = plugin::val('npc');
 
-    quest::debug("debug: $text, $dz_zone");
+    quest::debug("debug: $text, $client, $npc");
 }
