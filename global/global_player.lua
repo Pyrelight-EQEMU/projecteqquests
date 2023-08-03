6d1a80f2fb6cd43b2d82a5710fd0b8c29ce3dcc6
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-08-03 12:25:57 -0500

    test

--- global/global_player.lua
+++ global/global_player.lua
@@ -54,15 +54,14 @@ function refresh_instance_task(e)
     local dz = client:GetExpedition();
     local dz_id;
 
-    -- Check if a dynamic zone exists and if so, get its id
-    if dz.Valid then
-		eq.debug("We have a DZ");
-        dz_id = dz:GetZoneID();
+ 
+	dz_id = dz:GetZoneID();
+	if dz_id then
 		if not client:IsTaskActive(1000 + dz_id) then
 			eq.debug("Adding task");
 			client:AssignTask(1000 + dz_id);
-		end
-    end
+		end   
+	end
 
     -- Loop over the tasks from 1 to 999
     for i = 1, 999 do
