a80e81758f642a6177b17d5d752b63243c6cf511
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-08-03 13:17:22 -0500

    test

--- global/global_player.lua
+++ global/global_player.lua
@@ -59,7 +59,7 @@ function refresh_instance_task(e)
     -- Loop over the tasks from 1 to 999
     for i = 1, 999 do
         if client:IsTaskActive(1000 + i) then
-			eq.debug("Found a task " .. dz_id .. ", " .. reward);
+			eq.debug("Found a task");
 			if i ~= dz_id or reward == 0 then
             	client:FailTask(1000 + i)
 			end
