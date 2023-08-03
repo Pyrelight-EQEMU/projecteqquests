6888bd12f97629cca67a0791d88fd134031c1281
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-08-03 10:48:06 -0500

    test

--- everfrost/#a_Servant_of_Theralon.pl
+++ everfrost/#a_Servant_of_Theralon.pl
@@ -30,7 +30,7 @@ sub EVENT_SAY {
         $client->AssignTask(40);
         $client->CreateTaskDynamicZone(40, %zone_hash);
         $dz = $client->GetExpedition();
-        $client->MovePCDynamicZone($dz_zone);
+        $client->MovePCDynamicZone($dz);
     }
     plugin::ProcessInstanceDialog(%args);
 }
