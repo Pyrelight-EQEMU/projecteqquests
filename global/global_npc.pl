3d6309cabb5e2e1bac04272ee423ef8fff640a07
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-11 14:29:56 -0500

    test

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -236,14 +236,21 @@ sub GET_BAG_CONTENTS {
             # Instead of populating the hash directly, store the items in an array for sorting
             push @items_in_bag, {
                 id => $item_id,
+                damage => $owner->GetItemStat($item_id, "damage"),
+                hp => $owner->GetItemStat($item_id, "hp"),
+                ac => $owner->GetItemStat($item_id, "ac"),
                 quantity => 1,
                 augments => \@augments
             };
         }
     }
 
-    # Sort items by their ID
-    @items_in_bag = sort { $a->{id} <=> $b->{id} } @items_in_bag;
+    # Sort items based on the provided criteria
+    @items_in_bag = sort { 
+        $b->{damage} <=> $a->{damage} ||  # Sort by 'damage' first, in descending order
+        $b->{hp} <=> $a->{hp} ||          # Then by 'hp', in descending order
+        $b->{ac} <=> $a->{ac}             # Finally by 'ac', in descending order
+    } @items_in_bag;
 
     # Convert the sorted array to the desired hash format
     foreach my $item (@items_in_bag) {
@@ -255,6 +262,8 @@ sub GET_BAG_CONTENTS {
 
     return %new_bag_inventory;
 }
+
+
 
 sub APPLY_FOCUS {
     my $owner = $npc->GetOwner()->CastToClient();
