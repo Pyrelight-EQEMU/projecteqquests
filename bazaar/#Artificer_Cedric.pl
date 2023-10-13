3a3fd8d5f2794cd156e9d8c2f044090187325546
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-13 00:49:18 -0500

    test

--- bazaar/#Artificer_Cedric.pl
+++ bazaar/#Artificer_Cedric.pl
@@ -1,22 +1,59 @@
 sub EVENT_SAY {
-   my $clientName = $client->GetCleanName();
+    my $clientName = $client->GetCleanName();
 
-   my $link_enhance = "[".quest::saylink("link_enhance", 1, "enhancement of items")."]";
-   my $link_concentrated_mana_crystals = "[".quest::saylink("link_concentrated_mana_crystals", 1, "Concentrated Mana Crystals")."]";
-   my $link_show_me_your_equipment = "[".quest::saylink("link_show_me_your_equipment", 1, "show me your equipment")."]";
+    my $CMC_Points = $client->GetBucket("Artificer_CMC") || 0;
 
-   if($text=~/hail/i) {
-      if (!$client->GetBucket("CedricVisit")) {
-         plugin::NPCTell("Greetings, $clientName, I Cedric Sparkswall. I specialize in the $link_enhance, and have come to this grand center of commerce in order to ply my trade.");
-      } else {
-         plugin::NPCTell("Ah, it's you again, $clientName. How may I assist you with my $link_enhance today?");
-      }    
-   }
+    my $link_enhance = "[".quest::saylink("link_enhance", 1, "enhancement of items")."]";
+    my $link_concentrated_mana_crystals = "[".quest::saylink("link_concentrated_mana_crystals", 1, "Concentrated Mana Crystals")."]";
+    my $link_show_me_your_equipment = "[".quest::saylink("link_show_me_your_equipment", 1, "show me your equipment")."]";
+    my $link_aa_points = "[".quest::saylink("link_aa_points", 1, "temporal energy (AA Points)")."]";
+    my $link_platinum = "[".quest::saylink("link_platinum", 1, "platinum")."]";
+    my $link_siphon_10 = "[".quest::saylink("link_siphon_10", 1, "siphon 10 points")."]";
+    my $link_siphon_100 = "[".quest::saylink("link_siphon_100", 1, "siphon 100 points")."]";
 
-   elsif ($text eq "link_enhance") {
-      plugin::NPCTell("I can intensify the magic of certain equipment and weapons through the use of $link_concentrated_mana_crystals as well as an identical item to donate its aura. If you'd like to $link_show_me_your_equipment, can I tell you if you have equipment that is eligible for my services.");
-      $client->SetBucket("CedricVisit", 1);
-   }
+    if($text=~/hail/i) {
+        if (!$client->GetBucket("CedricVisit")) {
+            plugin::NPCTell("Greetings, $clientName, I Cedric Sparkswall. I specialize in the $link_enhance, and have come to this grand center of commerce in order to ply my trade.");
+        } else {
+            plugin::NPCTell("Ah, it's you again, $clientName. How may I assist you with my $link_enhance today?");
+        }    
+    }
+
+    elsif ($text eq "link_enhance") {
+        plugin::NPCTell("I can intensify the magic of certain equipment and weapons through the use of $link_concentrated_mana_crystals as well as an identical item to donate its aura. If you'd like to $link_show_me_your_equipment, can I tell you if you have equipment that is eligible for my services.");
+        $client->SetBucket("CedricVisit", 1);
+    }
+
+    elsif ($text eq "link_concentrated_mana_crystals") {
+        plugin::NPCTell("These mana crystals can be somewhat hard to locate. If you have trouble finding enough, I have a reasonable supply that I am willing to trade for your $link_aa_points or even mere $link_platinum.");
+        plugin::YellowText("You currently have $CMC_Points Concentrated Mana Crystals available.");
+    }
+
+    elsif ($text eq "link_platinum") {
+        plugin::NPCTell("If you want to buy $link_concentrated_mana_crystals with platinum, simply hand the coins to me. I will credit you with one crystal for each 500 coins.");
+    }
+
+    elsif ($text eq "link_platinum") {
+        plugin::NPCTell("If you want to buy $link_concentrated_mana_crystals with temporal energy, simply say the word. I can $link_siphon_10, $link_siphon_100, or $link_siphon_all and credit you with one crystal for each point removed.");
+    }
+
+    elsif ($text eq "link_siphon_10") {
+        if ($client->GetAAPoints() >= 10) {
+            $client->SetAAPoints($client->GetAAPoints() - 10);
+            plugin::NPCTell("Ahh. Excellent. I've added ten crystals under your name to my ledger.");
+        } else {
+            plugin::NPCTell("You do not have sufficient accumulated temporal energy for me to siphon that much from you!");
+        }
+    }
+
+    elsif ($text eq "link_siphon_100") {
+        if ($client->GetAAPoints() >= 100) {
+            $client->SetAAPoints($client->GetAAPoints() - 100);
+            plugin::NPCTell("Ahh. Excellent. I've added one hundred crystals under your name to my ledger.");
+        } else {
+            plugin::NPCTell("You do not have sufficient accumulated temporal energy for me to siphon that much from you!");
+        }
+    }
 }
 
 
