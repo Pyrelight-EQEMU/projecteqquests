be25ecde449420812b2fbf3ccacf5c6d9cf8ec94
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-15 15:03:27 -0500

    test

--- bazaar/#Artificer_Cedric.pl
+++ bazaar/#Artificer_Cedric.pl
@@ -54,6 +54,7 @@ sub EVENT_ITEM {
         }
 
         if ($earned_points > 0) {
+            plugin::YellowText("You currently have $cmc_avail Concentrated Mana Crystals available.");
             if ($total_money > 0) {
                 plugin::NPCTell("Ahh. Excellent. I've added $earned_points crystals under your name to my ledger. Here is your change!");
             } else {
