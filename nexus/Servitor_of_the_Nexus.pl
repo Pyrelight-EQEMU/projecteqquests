3b211d8b97e2e86bd3e0d0778ad18dbd8de8a316
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 17:38:38 -0500

    test

--- nexus/Servitor_of_the_Nexus.pl
+++ nexus/Servitor_of_the_Nexus.pl
@@ -14,18 +14,18 @@ sub POPUP_DISPLAY {
   my $green  = plugin::PWColor("Green"); 
   my $red = plugin::PWColor("Red");
 
-  my $desc = "Pyrelight is a single box server, meant to offer a challenging soloable experience for veteran players and an alternative take on the 'solo progression' mold.
-              $red PLEASE READ THIS ENTIRE WINDOW</c>.
+  my $desc = "Pyrelight is a single box server, meant to offer a challenging soloable experience for veteran players and an alternative take on the 'solo progression' mold.              
               For more detailed information and ongoing discussion, please join the server discord (". plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA") .").<br><br>";
 
-  my $feature_header = $yellow . "Features</c><br>";
+  my $feature_header = $yellow . "Features</c><br><br>";
 
   my $feature_desc = "$green Multiclassing</c> - You may unlock additional classes for your character. The first additional unlock is obtained at level 20. Each alternate 
                       class should be thought of as an alternate character in many respects, each has an independent Levels & Experience, Equipment, and unspent AA. However,
                       your Inventory, Skills, spent AA, and progression achievements are automatically shared between all of your classes. Additionally, your Spells may be 
                       shared with your alternate classes through the Spellshaper in the Bazaar. This system has some ";
 
-  my $text = $desc .
+  my $text = quest::popupcentermessage("$red PLEASE READ THIS ENTIRE WINDOW</c>.").
+             $desc .
              quest::popupcentermessage($feature_header) .
              $feature_desc ;  
 
