a0f870f336b33c5207003a3d8ca5a7d2d09312cc
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 16:56:10 -0500

    test

--- nexus/#Grand_Arcanist_Eithan.pl
+++ nexus/#Grand_Arcanist_Eithan.pl
@@ -5,39 +5,30 @@ sub EVENT_SAY {
 
   if ($text=~/hail/i) {     
       if ($progress <= 0) {
-        plugin::NPCTell("Hail, $clientName. You must be the latest bit of [dimensional flotsam] to wash up.");
+        my $adventurers_soul = quest::varlink(200000);
+        plugin::NPCTell("Hail, $clientName. I sense the [$adventurers_soul] growing within you. Welcome to the Nexus, the best place in all the realms to start
+                        your journey to [nurture] it.");
       } else {
         plugin::NPCTell("It is good to see you again, $clientName. I don't have anything for you right now, but return to me when you've gained 
                          more experience. I will have work for you.");
       }
-  } elsif ($text=~"mao1a") { #dimensional flotsam
-    plugin::NPCTell("Apologies! Allow my to introduce myself; I am Eithan, the head archaeologist of the Wayfarer's Brotherhood. This site is an ". 
-                    "[".quest::saylink("mao1b",1,"ancient teleportation complex")."] built by the Combine prior to their decline. Unfortunately, it seems".
-                    "to act as something of a magnet for dimensional travelers. Periodically a stranger such as yourself will turn up - no memory of from ".
-                    "whence they came and [".quest::saylink("mao1c",1,"no clue how to return")."].");
-  } elsif ($text=~"mao1b") { #ancient teleportation complex
-    plugin::NPCTell("The Nexus was built long ago, and while it has fallen into disrepair as a teleportation hub, it has remained a center of commerce for the ".
-                    "inhabitants of Luclin, with both The Bazaar and the City of Shadowhaven nearby.");
-  } elsif ($text=~"mao1c") { #no clue how to return
-    plugin::NPCTell("I know that this is probably quite the shock, and you are of course welcome to investigate your own origins, but ".
-                    "[".quest::saylink("mao1d",1,"the Brotherhood")."] will be of little assistance. We have been unsuccessful with previous Travelers, and expect ".
-                    "you to be no different. We can help you in making home for yourself in this world, however.");
-  } elsif ($text=~"mao1d") { #the Brotherhood
-    plugin::NPCTell("The Wayfarer's Brotherhood is an organization of Adventuers who have banded together to delve into the dangerous and forgotten secret places ".
-                    "of Norrath and beyond. We recently were able to reactivate the Great Spires of Norrath, reconnecting the world to the moon Luclin, where you stand now. ".
-                    "I've also heard rumors that our leader, Morden Rasp, has set out on an expedition to lost Taelosia. I will let the staff here know to allow you access ".
-                    "to the [".quest::saylink("mao1e", 1, "teleportation network")."]. I suggest that you venture out and find a place for yourself in this world, and once ".
-                    "you've gained some more experience, I'll have some work for you." );
-  } elsif ($text=~"mao1e") { #teleportation network
+
+  } elsif ($text=~"/nurture/i") { 
+    plugin::NPCTell("That tiny kernel within you will not sustain you yet, much less bring you the wealth and power you undoubtedly chase. You may first cause it to 
+                    grow by simple exploration. There are other benefits, namely your access to the [Nexus Teleportation Network] will expand as your Soul does.");
+  }
+  
+  elsif ($text=~"exus Teleportation Network") { #teleportation network
     plugin::NPCTell("There are wizards of the brotherhood located near each of the minor spires within the Nexus complex, they can assist you with traveling to the ".
-                    "continents to which they are attuned. Only the existing Combine spires will allow a blind transit, however. There are other sites with weak ".
-                    "dimensional barriers to which you can personally be attuned, though - you'll simply need to reach them on your own first." );
+                    "continents to which they are attuned. Only the existing Combine spires, or locations that you are already strongly familiar with, will allow a 
+                    blind transit, however. There are other sites with weak dimensional barriers to which you can personally be attuned, though - you'll simply need 
+                    to reach them on your own first." );
     
     my $acctMoneyFlagKey = $client->AccountID() . "-InitMoneyFlag";
     my $acctMoneyFlagValue = quest::get_data($acctMoneyFlagKey);
     if ($acctMoneyFlagValue <= 10 && $progress <= 0) {
       plugin::NPCTell("Also, take these coins. You'll need them to get started, and you can pay me back sometime if your pride demands it.");
-      quest::givecash(0, 0, 0, 10);
+      quest::givecash(0, 0, 0, 100);
       quest::set_data($acctMoneyFlagKey, ++$acctMoneyFlagValue);
     }
     
