96985fe73ef1790b986350bc037ac7557c8a4cf5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 17:20:33 -0500

    test

--- nexus/Servitor_of_the_Nexus.pl
+++ nexus/Servitor_of_the_Nexus.pl
@@ -14,18 +14,18 @@ sub POPUP_DISPLAY {
   my $green  = plugin::PWColor("Green"); 
 
   my $discord = "Server Discord: " . plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA") . "<br><br>";
-  my $header = $yellow . plugin::PWAutoCenter("Welcome to Pyrelight!") . "</c><br><br>";
 
   my $desc = "Pyrelight is a solo-balanced server, meant to offer a challenging experience for veteran players and an alternative take on the 'solo progression' mold.<br><br>
               For more detailed information and ongoing discussion, please join the server discord. The " . $green . "#server-info</c>, 
               " . $green . "#faq</c>, and " . $green . "#changelog</c> channels may be particularly interesting.<br><br>";
 
-  my $feature_header = $yellow . "Features:</c>";
+  my $feature_header = $yellow . "Features</c><br>";
 
-  my $text = $header .
-             $discord .
-             $desc . quest::popupbreak() .
+  my $feature_desc = ""
+
+  my $text = $discord .
+             $desc .
              $feature_header .
              $desc ;  
-  quest::popup('', $text);
+  quest::popup('Welcome to Pyrelight', $text);
 }
