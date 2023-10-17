78265fbcc7fc11a6abd374b6f036420d2dffb720
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 00:28:47 -0500

    test

--- global/global_player.pl
+++ global/global_player.pl
@@ -52,8 +52,8 @@ sub EVENT_CONNECT {
 
         # Store the current time
         $client->SetBucket("LastLoginTime", $today_6am);
-        #$client->SummonItem(40605, 1);
-        #plugin::YellowText("You have been granted a daily log-in reward!");
+        $client->SummonItem(40605, 1);
+        plugin::YellowText("You have been granted a daily log-in reward!");
     }
 
     #quest::debug($client->CharacterID());
