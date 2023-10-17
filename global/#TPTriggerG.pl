4fa7e13e7ddcbd5c1ea22787c07b8706535b04e5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 13:16:18 -0500

    test

--- global/#TPTriggerG.pl
+++ global/#TPTriggerG.pl
@@ -1,36 +1,35 @@
 sub EVENT_SPAWN {
+    $x = $npc->GetX();
+    $y = $npc->GetY();
+    $z = $npc->GetZ();
 
-        $x = $npc->GetX();
-        $y = $npc->GetY();
-        $z = $npc->GetZ();
-
-        quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+    quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
 sub EVENT_ENTER {
-	
-	quest::debug("Attempting to update attunement point...");	
-	
-	my @tokens = split /:/, $npc->GetLastName();
+    quest::debug("Attempting to update attunement point...");
 
-        my $charKey = $client->CharacterID() . "-TL-" . $tokens[0];
-        my $charTL  = quest::get_data($charKey);
+    my @tokens = split /:/, $npc->GetLastName();
+    my $suffix = $tokens[0];
+    my $characterID = $client->CharacterID();
 
-	my $TLDesc = "";	
+    my $TLDesc = "";
+    if ($tokens[1] eq "") {
+        $TLDesc = quest::GetZoneLongNameByID($npc->GetZoneID());
+    } else {
+        $TLDesc = quest::GetZoneLongNameByID($npc->GetZoneID()) . " " . $tokens[1];
+    }
 
-	if ($tokens[1] eq "") {
-		$TLDesc = quest::GetZoneLongNameByID($npc->GetZoneID());
-	} else {
-		$TLDesc = quest::GetZoneLongNameByID($npc->GetZoneID()) . " " . $tokens[1];
-	}
+    my $locData = [quest::GetZoneShortName($npc->GetZoneID()), $TLDesc, $npc->GetX(), $npc->GetY(), $npc->GetZ(), $npc->GetHeading()];
 
-        my $locString = ":" . quest::GetZoneShortName($npc->GetZoneID()) . "," . $TLDesc . "," . $npc->GetX() . "," . $npc->GetY() . "," . $npc->GetZ() . "," . $npc->GetHeading();
-		
-        if (index($charTL, $TLDesc) == -1 and !($tokens[0] eq "")) {
-                quest::message(15,"You feel a tug on your soul. Your have become attuned to this location.");
-                quest::ding();
-                quest::set_data($charKey, $charTL . $locString);
-        } elsif ($tokens[0] eq "") {
-		quest::debug("Configuration Error.");
-	}
+    if (!plugin::has_zone_entry($characterID, $TLDesc, $suffix) and !($suffix eq "")) {
+        quest::message(15, "You feel a tug on your soul. Your have become attuned to this location.");
+        quest::ding();
+
+        # Adding the new attunement location to the character's data
+        plugin::add_zone_entry($characterID, $TLDesc, $locData, "-" . $suffix);
+
+    } elsif ($suffix eq "") {
+        quest::debug("Configuration Error.");
+    }
 }
