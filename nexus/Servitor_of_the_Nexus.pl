98a808ef58e565a9bf43ee1e8ea84171d5300cd8
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 17:24:22 -0500

    test

--- nexus/Servitor_of_the_Nexus.pl
+++ nexus/Servitor_of_the_Nexus.pl
@@ -13,9 +13,9 @@ sub POPUP_DISPLAY {
   my $yellow = plugin::PWColor("Yellow");
   my $green  = plugin::PWColor("Green"); 
 
-  my $desc = "Pyrelight is a solo-balanced server, meant to offer a challenging experience for veteran players and an alternative take on the 'solo progression' mold.<br><br>
-              For more detailed information and ongoing discussion, please join the server discord (". plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA") ."). The " . $green . "#server-info</c>, 
-              " . $green . "#faq</c>, and " . $green . "#changelog</c> channels may be particularly interesting.<br>";
+  my $desc = "Pyrelight is a solo-balanced server, meant to offer a challenging experience for veteran players and an alternative take on the 'solo progression' mold.
+              For more detailed information and ongoing discussion, please join the server discord 
+              (". plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA") .").<br><br>";
 
   my $feature_header = $yellow . "Features</c><br>";
 
