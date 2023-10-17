78250cf0b1e71892d0c423e695227fbbd64c8d3d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 11:32:57 -0500

    test

--- global/items/AdvSoul.pl
+++ global/items/AdvSoul.pl
@@ -1,7 +1,6 @@
-sub EVENT_SCALE_CALC {    
-    quest::debug("Scaling AdvSoul");
-
-    my $attunements = plugin::get_total_attunements($client);
+sub EVENT_SCALE_CALC {
+    my $attunements = plugin::get_total_attunements($client);    
+    quest::debug("Scaling AdvSoul: $attunements");
 
     $questitem->SetScale($attunements/5);
 }
