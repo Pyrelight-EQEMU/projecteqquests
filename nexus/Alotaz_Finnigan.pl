b15b72313b824995eee5f6c4e5738bb1a7e1a76b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 14:03:42 -0500

    test

--- nexus/Alotaz_Finnigan.pl
+++ nexus/Alotaz_Finnigan.pl
@@ -25,6 +25,6 @@ sub EVENT_SAY {
             $client->Message(257, "-> " . quest::saylink($t, 0, $t));
         }
     } elsif (exists($teleport_zones->{$text})) {
-        $client->MovePC(quest::GetZoneID($teleport_zones->{$text}[0]), $teleport_zones->{$text}[2], $teleport_zones->{$text}[3], $teleport_zones->{$text}[4], $teleport_zones->{$text}[5]);
+        $client->MovePC(quest::GetZoneID($teleport_zones->{$text}[0]), $teleport_zones->{$text}[1], $teleport_zones->{$text}[2], $teleport_zones->{$text}[3], $teleport_zones->{$text}[4]);
     }
 }
