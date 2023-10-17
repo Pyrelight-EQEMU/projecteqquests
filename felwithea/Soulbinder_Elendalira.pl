872833ad4b1a4eee7b3d149a18c802ef87c58842
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 15:56:03 -0500

    test

--- felwithea/Soulbinder_Elendalira.pl
+++ felwithea/Soulbinder_Elendalira.pl
@@ -1,15 +1,8 @@
--- Converted to .lua by Speedz
+#generic soulbinder quest
+sub EVENT_SAY { 
+	plugin::soulbinder_say($text);
+}
 
-function event_say(e)
-	if(e.message:findi("Hail")) then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". When a hero of our world is slain their soul returns to the place it was last bound and the body is reincarnated. As a member of the Order of Eternity  it is my duty to [bind your soul] to this location if that is your wish.");
-	elseif(e.message:findi("bind my soul")) then
-		e.self:Say("Binding your soul. You will return here when you die.");
-		e.self:CastSpell(2049,e.other:GetID(),0,1); -- Spell: Bind Affinity
-	end
-end
-
-function event_trade(e)
-	local item_lib = require("items");
-	item_lib.return_items(e.self, e.other, e.trade)
-end
+sub EVENT_SIGNAL {
+	quest::emote("smiles brightly 'Ah, you are so kind to ask.  I'll have a light beer, if it's convenient.  Hey, when do you get off work?'");
+}
