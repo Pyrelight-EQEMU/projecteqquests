91bfa32f5864c3cd5f991b52e8df019b81e886e5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 17:46:38 -0500

    test

--- nexus/Servitor_of_the_Nexus.pl
+++ nexus/Servitor_of_the_Nexus.pl
@@ -13,6 +13,7 @@ sub POPUP_DISPLAY {
   my $yellow = plugin::PWColor("Yellow");
   my $green  = plugin::PWColor("Green"); 
   my $red = plugin::PWColor("Red");
+  my $purple = plugin::PWColor("Purple");
 
   my $desc = "Pyrelight is a single box server, meant to offer a challenging soloable experience for veteran players and an alternative take on the 'solo progression' mold.              
               For more detailed information and ongoing discussion, please join the server 
@@ -23,7 +24,14 @@ sub POPUP_DISPLAY {
   my $feature_desc = "$green Multiclassing</c> - You may unlock additional classes for your character. The first additional unlock is obtained at level 20. Each alternate 
                       class should be thought of as an alternate character in many respects, each has an independent Levels & Experience, Equipment, and unspent AA. However,
                       your Inventory, Skills, spent AA, and progression achievements are automatically shared between all of your classes. Additionally, your Spells may be 
-                      shared with your alternate classes through the Spellshaper in the Bazaar. This system has some ";
+                      shared with your alternate classes through the Spellshaper in the Bazaar. This system has some strange consquences - Look at the Required Level on 
+                      spell scrolls, and your Skills window will show any skill you could possibly earn, regardless of your class.<br><br>
+                      $green MQ2-Style Quality of Life</c> - While$red MQ2 is not allowed on this server</c>, many of the reasons why you would want it on a single-box server
+                      are built-in to the client. You can see mobs on your map (names become visible with Tracking skill or custom Situational Awareness AA), you can see info
+                      on spells and the value of items on inspect windows.<br><br>
+                      $green Heroic Stats</c> - Most direct increases to character strength are accomplished by way of abdundant Heroic stats on items. Your [$purple Adventurer's Soul</c>]
+                      
+                      ";
 
   my $text = quest::popupcentermessage("$red NEW PLAYERS PLEASE READ THIS WINDOW</c><br>") .
              $desc .
