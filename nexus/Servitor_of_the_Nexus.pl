abedc386ba97f08c9a496bb7d5c1ea538740bd9b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 17:39:00 -0500

    test

--- nexus/Servitor_of_the_Nexus.pl
+++ nexus/Servitor_of_the_Nexus.pl
@@ -24,7 +24,7 @@ sub POPUP_DISPLAY {
                       your Inventory, Skills, spent AA, and progression achievements are automatically shared between all of your classes. Additionally, your Spells may be 
                       shared with your alternate classes through the Spellshaper in the Bazaar. This system has some ";
 
-  my $text = quest::popupcentermessage("$red PLEASE READ THIS ENTIRE WINDOW</c>.").
+  my $text = quest::popupcentermessage("$red PLEASE READ THIS ENTIRE WINDOW</c>") .
              $desc .
              quest::popupcentermessage($feature_header) .
              $feature_desc ;  
