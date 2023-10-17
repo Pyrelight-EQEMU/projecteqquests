2fbaa4d4780bb816feb054cc81be5efb0d9cd8d7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-17 16:19:49 -0500

    test

--- plugins/soulbinders.pl
+++ plugins/soulbinders.pl
@@ -2,6 +2,7 @@
 
 sub soulbinder_say {
 	my $text = shift;
+	my $client     = plugin::val('client');
 	my $clientname = $client->GetCleanName();
 	if($text=~/hail/i){
 		quest::say("Greetings $clientname . When a hero of our world is slain their soul returns to the place it was last bound and the body is reincarnated. As a member of the Order of Eternity  it is my duty to [bind your soul] to this location if that is your wish. I can also [attune] you to this place for the purposes of teleportation from The Nexus.");
