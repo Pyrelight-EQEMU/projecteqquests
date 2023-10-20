bd12f2b3c88c421cca11d9df02631b126e9193c2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-20 17:14:31 -0500

    test

--- bazaar/#Gemcarver_Avelyn.pl
+++ bazaar/#Gemcarver_Avelyn.pl
@@ -42,7 +42,7 @@ sub EVENT_ITEM {
                   } else {
                      my @augs = get_augs($base_id);
                      my $cost = get_upgrade_cost($base_id) * (scalar @augs) + plugin::GetTotalLevels($client);
-                     if (scalar @augs) {
+                     if (scalar @augs > 0) {
                            my $response = "We have some interesting components here. ";                           
                            if (@augs == 1) {
                               $response .= "I see a " . quest::varlink($augs[0]) . ".";
