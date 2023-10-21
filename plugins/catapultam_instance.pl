72c210e219edb7716aa0ba1bcf823c5fefdd1188
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-21 16:06:47 -0500

    test

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -342,7 +342,7 @@ sub upgrade_item_tier {
 
         my $target_tier = min(10, $tier + $curtier);
         my $target_item = $base_id + (1000000 * $tier);
-
+        quest::debug("base: $base_id, target: $target_item");
         if (plugin::item_exists_in_db($target_item)) {
             if ($corpse && $corpse->GetCount($item_id)) {            
                 $corpse->RemoveItemByID($item_id);
