01c2793dbeba725e7739250d98e0ec7730b2ad2b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-21 13:01:00 -0500

    test

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -438,9 +438,7 @@ sub ModifyInstanceNPC
     my $reward      = $info_bucket{'reward'};
 
     my $difficulty_modifier = 1 + ($modifier * $difficulty);
-
-    quest::debug("difficulty_modifier: $difficulty_modifier, difficulty: $difficulty, modifier: $modifier");
-
+    
     # Get initial mob stat values
     my @stat_names = qw(max_hp min_hit max_hit atk mr cr fr pr dr spellscale healscale accuracy avoidance heroic_strikethrough);  # Add more stat names here if needed
     my %npc_stats;
