188c2710cfd0e8946f266d2f8f98b1655fe4e3d8
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-21 14:47:48 -0500

    facs

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -347,8 +347,7 @@ sub upgrade_item_tier {
 
         $tier = $current_tier + $tier;
         if (plugin::item_exists_in_db($add_item) && $add_item != $item_id) {            
-            quest::debug("Removing: $item_id, Adding: $add_item");
-            if ($entity->)
+            quest::debug("Removing: $item_id, Adding: $add_item");            
             $entity->RemoveItemByID($item_id);
             $entity->AddItem($add_item, 1);
         }
