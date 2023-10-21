dce1ec3a0a8acfd59f4a9ab03f98937523dabd7d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-21 02:10:25 -0500

    test

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -352,22 +352,7 @@ sub ModifyInstanceNPC
         }
     }
 
-    foreach my $stat (@stat_names) {
-        $npc_stats_perlevel{$stat} = ($npc_stats{$stat} / $npc->GetLevel());
-    }
 
-    # Rescale Levels
-    if ($npc->GetLevel() < ($min_level - 6)) {
-        my $level_diff = $min_level - 6 - $npc->GetLevel();
-
-        $npc->SetLevel($npc->GetLevel() + $level_diff);
-        foreach my $stat (@stat_names) {
-            # Skip processing for 'spellscale' and 'healscale'
-            next if ($stat eq 'spellscale' or $stat eq 'healscale');
-
-            $npc->ModifyNPCStat($stat, $npc->GetNPCStat($stat) + ceil($npc_stats_perlevel{$stat} * $level_diff));
-        }      
-    }
 
     #Recale stats
     if ($difficulty > 0) {
