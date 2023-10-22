9f301021b6048f5122f090cbc7d1e8e5c3927ecc
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-22 13:45:00 -0500

    test

--- bazaar/#Sorcerer_Theralon.pl
+++ bazaar/#Sorcerer_Theralon.pl
@@ -3,9 +3,18 @@ use DBD::mysql;
 
 sub EVENT_SAY
 {
-    my $charname = $client->GetCleanName();
-    my $progress = $client->GetBucket("MAO-Progress") || 0;
-    my $met_befo = $client->GetBucket("TheralonIntro") || 0;
+    my $charname            = $client->GetCleanName();
+    my $progress            = $client->GetBucket("MAO-Progress") || 0;
+    my $met_befo            = $client->GetBucket("TheralonIntro") || 0;
+    my @locked_classes      = plugin::GetLockedClasses($client);
+    my %unlocked_class      = plugin::GetUnlockedClasses($client);
+    my $total_classes       = scalar(keys %unlocked_class);
+    my $unlocksAvailable    = $client->GetBucket("ClassUnlocksAvailable") || 0;
+    my @costs = (0, 0, 50, 200, 500, 1000, 2000, 3000, 4000, 5000);
+    my $expRate             = $client->GetEXPModifier(0);
+    my $percentage_expRate  = int($expRate * 100);
+    my $FoS_Token           = plugin::Get_FoS_Tokens($client);
+    my $FoS_Heroic_Token    = plugin::Get_FoS_Heroic_Tokens($client);
           
     if ($text=~/hail/i) {
         if ($progress < 3) {
@@ -41,23 +50,16 @@ sub EVENT_SAY
     elsif ($text=~/tokens/i && $progress > 3 && $met_befo) {
         plugin::NPCTell("Ah, tokens! The universal language of favors. 
                         Let me show you the marvels they can bring forth.");
+        plugin::Display_FoS_Tokens($client);
+        plugin::Display_FoS_Heroic_Tokens($client);
         plugin::PurpleText("- [Class Unlocks] - [Special Abilities] - [Equipment] -");
     }
 
     elsif ($text=~/Class Unlocks/i && $progress > 3 && $met_befo) {
-        my @locked_classes      = plugin::GetLockedClasses($client);
-        my %unlocked_class      = plugin::GetUnlockedClasses($client);
-        my $total_classes       = scalar(keys %unlocked_class);
-        my $unlocksAvailable    = $client->GetBucket("ClassUnlocksAvailable") || 0;
-        my @costs = (0, 0, 50, 200, 500, 1000, 2000, 3000, 4000, 5000);
 
-        my $expRate             = $client->GetEXPModifier(0);
-        my $percentage_expRate  = int($expRate * 100);
         if (!$unlocksAvailable) {
-            my $link_confirm_unlock = "- [".quest::saylink("link_confirm_unlock", 1, "UNLOCK")."] - I confirm that I understand that I will recieve a permanent XP/AAXP Penalty.";
-
-            plugin::PurpleText("WARNING: You will receive a permanent 25%% multiplicative XP/AAXP penalty for each additional unlock that you purchase.");            
-            plugin::PurpleText("You are currently earning $percentage_expRate%% of normal XP, and have $total_classes classes unlocked.");
+            my $link_confirm_unlock = "- [".quest::saylink("link_confirm_unlock", 1, "UNLOCK")."] ($costs[$total_classes] Feat of Strength Tokens) - I confirm that I understand that I will recieve a permanent XP/AAXP Penalty.";
+            plugin::PurpleText("WARNING: You will receive a permanent 25%% multiplicative XP/AAXP penalty for each additional unlock that you purchase. You are currently earning $percentage_expRate%% of normal XP, and have $total_classes classes unlocked.");            
             plugin::PurpleText("$link_confirm_unlock");
         } else {
             # Other code or conditions can go here.
