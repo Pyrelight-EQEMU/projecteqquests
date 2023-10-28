f48569d6db1c6c44787e6e9ebe9add4ffc37ade5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-10-28 09:40:54 -0500

    introduce lavastorm FoS

---
+++ lavastorm/#a_Servant_of_Theralon.pl
@@ -0,0 +1,11 @@
+# This script should be copy-pasted and reconfigured for each instance master
+
+my $explain_details = "Within the caverns ahead is the lair of the legendary Red Dragon of Antonica - Lord Nagafen. Prove your strength to Master Theralon by slaying her and her minions.";
+
+my $zone_name       = 'soldungb';
+my $reward          = 1;
+my @task_id         = (44, 45, 46);
+
+sub EVENT_SAY {
+    plugin::HandleSay($client, $npc, $zone_name, $explain_details, $reward, @task_id);
+}
