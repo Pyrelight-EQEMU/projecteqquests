6a4ea725d01999594b9107e4ff7b482e5448dd7e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-01 13:10:35 -0500

    test

--- global/spells/default.pl
+++ global/spells/default.pl
@@ -10,9 +10,6 @@ sub EVENT_SPELL_FADE {
     if ($npc && quest::IsCharmSpell($spell_id)) {
         my $name = $npc->GetCleanName();
         quest::debug("I am: $name, and I am a recovering charm pet.");
-        plugin::SpawnInPlaceByEnt($npc);
-        if ($npc) {
-            $npc->Kill();
-        }
+        plugin::SpawnInPlaceByEnt($npc);        
     }
 }
