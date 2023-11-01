f49f05ce8cf3ed80ef114f3529200d764a563f6d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-01 15:32:05 -0500

    test

--- global/spells/default.pl
+++ global/spells/default.pl
@@ -1,14 +1,34 @@
 sub EVENT_SPELL_EFFECT_NPC {
+    my $client = $entity_list->GetClientByID($caster_id);
     if (quest::IsCharmSpell($spell_id) && $entity_list->GetClientByID($caster_id)) {
         my $name = $npc->GetCleanName();
         quest::debug("I am: $name, and this is my life now.");
 
         my @stat_names = qw(max_hp min_hit max_hit atk mr cr fr pr dr spellscale healscale accuracy avoidance heroic_strikethrough);
+        my %npc_stats;
+        my $npc_stats_perlevel;
 
         foreach my $stat (@stat_names) {
             if ($npc->EntityVariableExists($stat)) {
-                $npc->ModifyNPCStat($stat, $npc->GetEntityVariable($stat));
+                $npc_stats{$stat} = $npc->GetEntityVariable($stat);
+            } else {
+                $npc_stats{$stat} = $npc->GetNPCStat($stat);
+                $npc->SetEntityVariable($stat, $npc_stats{$stat});
             }
+        }
+
+        foreach my $stat (@stat_names) {
+            $npc_stats_perlevel{$stat} = ($npc_stats{$stat} / $npc->GetLevel());
+        }
+
+        # Rescale Levels
+        if ($npc->GetLevel() < ($min_level - 6)) { 
+            $npc->SetLevel($client->GetLevel());
+            foreach my $stat (@stat_names) {
+                # Skip processing for 'spellscale' and 'healscale'
+                next if ($stat eq 'spellscale' or $stat eq 'healscale');
+                $npc->ModifyNPCStat($stat, $npc->GetNPCStat($stat) + ceil($npc_stats_perlevel{$stat} * $level_diff));
+            }      
         }
     }
 }
