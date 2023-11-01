1b9f75e71e9e0e4904805c825bfde12d0812c6be
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-01 12:28:03 -0500

    t

--- global/spells/default.pl
+++ global/spells/default.pl
@@ -1,21 +1,3 @@
-sub CHECK_CHARM_STATUS
-{
-    if ($npc->Charmed() and not plugin::REV($npc, "is_charmed")) {     
-        my @lootlist = $npc->GetLootList();
-        my @inventory;
-        foreach my $item_id (@lootlist) {
-            my $quantity = $npc->CountItem($item_id);
-            push @inventory, "$item_id:$quantity";
-        }
-
-        my $data = @inventory ? join(",", @inventory) : "EMPTY";
-        plugin::SEV($npc, "is_charmed", $data);
-
-    } elsif (not $npc->Charmed() and plugin::REV($npc, "is_charmed")) {        
-        plugin::SpawnInPlaceByEnt($npc);
-    }
-}
-
 sub EVENT_SPELL_FADE {    
     if ($npc && quest::IsCharmSpell($spell_id)) {
         my $name = $npc->GetCleanName();
