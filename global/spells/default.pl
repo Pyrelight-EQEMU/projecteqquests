2f50caded36cf5e6c6aa736bb536634c07dd13c7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-01 12:55:36 -0500

    test

--- global/spells/default.pl
+++ global/spells/default.pl
@@ -1,3 +1,11 @@
+sub EVENT_SPELL_EFFECT_NPC {
+    if (quest::IsCharmSpell($spell_id) && $entity_list->GetClientByID($caster_id)) {
+        my $name = $npc->GetCleanName();
+        quest::debug("I am: $name, and this is my life now.");
+        $npc->ScaleNPC($npc->GetLevel(), 1);
+    }
+}
+
 sub EVENT_SPELL_FADE {    
     if ($npc && quest::IsCharmSpell($spell_id)) {
         my $name = $npc->GetCleanName();
