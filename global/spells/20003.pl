d8652b2dd60ddb0db77bfd3b85ed518076d8c193
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-04 13:33:22 -0500

    stop fading buffs from clients

--- global/spells/20003.pl
+++ global/spells/20003.pl
@@ -5,8 +5,6 @@ sub EVENT_SPELL_EFFECT_CLIENT {
         if ($mclass==$tclass) {
             $client->Message(13, "Ability Failed. You are already a ". quest::getclassname($tclass));
         } else {        
-            $client->BuffFadeAll();
-            $client->RemovePet();
             quest::permaclass($tclass);
         }
     }
