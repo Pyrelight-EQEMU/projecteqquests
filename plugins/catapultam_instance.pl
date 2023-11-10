ee8580e9b135c48e19abf381c5644f968d3cfa66
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-10 12:27:15 -0600

    f

--- plugins/catapultam_instance.pl
+++ plugins/catapultam_instance.pl
@@ -467,16 +467,11 @@ sub ModifyInstanceNPC
         $npc->ModifyNPCStat('avoidance', $npc->GetNPCStat('avoidance') + (ceil(rand() * 100) * $difficulty) + ($difficulty * 10));
         $npc->ModifyNPCStat('heroic_strikethrough', $npc->GetNPCStat('heroic_strikethrough') + max(0,(ceil(rand() * 25) * ($difficulty - 10)) + $difficulty));
 
-        my $new_attack_delay = $npc->GetNPCStat('attack_delay') - ceil(rand() * $difficulty * 25);
+        my $new_attack_delay = $npc->GetNPCStat('attack_delay') - ceil(rand() * $difficulty * 100);
         quest::debug("ATTACK DELAY MOD: $new_attack_delay");
 
         $npc->ModifyNPCStat('slow_mitigation', $npc->GetNPCStat('slow_mitigation') + floor(rand() * $difficulty));
-        $npc->ModifyNPCStat("attack_delay", 
-                            ceil(max(
-                                1000, 
-                                $npc->GetNPCStat('attack_delay') - ceil(rand() * $difficulty * 25)
-                            ) / 100)
-                        );
+        $npc->ModifyNPCStat("attack_delay", $new_attack_delay);
     }
 
     $npc->Heal();
